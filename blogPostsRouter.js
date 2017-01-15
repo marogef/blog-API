a49cf1e6f16aea61508d07dd0b4b5fd21277021a
Faras Maroge <Goldencrane33@yahoo.com>
2017-01-15 00:35:44 -0500

    added the router, create, delete, update, and get requirements

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -23,61 +23,76 @@ function StorageException(message) {
     this.message = message;
     this.name = "StorageException";
 }
+router.get('/', (req, res) => {
+    res.json(BlogPosts.get());
+});
 
 const BlogPosts = {
-    create: function (title, content, author, publishDate) {
-        const post = {
-            id: uuid.v4(),
-            title: title,
-            content: content,
-            author: author,
-            publishDate: publishDate || Date.now()
-        };
-        this.posts.push(post);
-        return post;
-    },
-    get: function (id = /blog-posts) {
-        // if id passed in, retrieve single post,
-        // otherwise send all posts.
-        if (id !== /blog-posts) {
-            return this.posts.find(post => post.id === id);
-        }
-        // return posts sorted (descending) by
-        // publish date
-        return this.posts.sort(function (a, b) {
-            return b.publishDate - a.publishDate
+        create: function (title, content, author, publishDate) {
+            const post = {
+                id: uuid.v4(),
+                title: title,
+                content: content,
+                author: author,
+                publishDate: publishDate || Date.now()
+            };
+            this.posts.push(post);
+            return post;
+        },
+        get: function (id = /blog-posts) {
+            // if id passed in, retrieve single post,
+            // otherwise send all posts.
+            if (id !== /blog-posts) {
+                return this.posts.find(post => post.id === id);
+            }
+            // return posts sorted (descending) by
+            // publish date
+            return this.posts.sort(function (a, b) {
+                return b.publishDate - a.publishDate
+            });
+        },
+        // when PUT request comes in with updated item, ensure has
+        // required fields. also ensure that item id in url path, and
+        // item id in updated item object match. if problems with any
+        // of that, log error and send back status code 400. otherwise
+        // call `ShoppingList.update` with updated item.
+        app.put('/blog-posts/:id', jsonParser, (req, res) => {
+            const requiredFields = [
+                'id', 'title', 'content', 'author', 'publishDate'];
+            for (let i = 0; i < requiredFields.length; i++) {
+                const field = requiredFields[i];
+                if (!(field in req.body)) {
+                    const message = `Missing \`${field}\` in request body`
+                    console.error(message);
+                    return res.status(400).send(message);
+                }
+            }
+            if (req.params.id !== req.body.id) {
+                const message = (
+                    `Request path id (${req.params.id}) and request body id `
+                    `(${req.body.id}) must match`);
+                console.error(message);
+                return res.status(400).send(message);
+            }
+            console.log(`Updating shopping list item \`${req.params.id}\``);
+            const updatedItem = ShoppingList.update({
+                id: req.params.id,
+                title: req.body.title,
+                content: req.body.content,
+                author: req.body.author,
+                publishDate: req.body.publishDate
+            });
+            res.status(204).json(updatedItem);
         });
-    },
-    delete: function (id) {
-        const postIndex = this.posts.findIndex(
-            post => post.id === id);
-        if (postIndex > -1) {
-            this.posts.splice(postIndex, 1);
-        }
-    },
-    update: function (updatedPost) {
-        const {
-            id
-        } = updatedPost;
-        const postIndex = this.posts.findIndex(
-            post => post.id === updatedPost.id);
-        if (postIndex === -1) {
-            throw StorageException(
-                `Can't update item \`${id}\` because doesn't exist.`)
-        }
-        this.posts[postIndex] = Object.assign(
-            this.posts[postIndex], updatedPost);
-        return this.posts[postIndex];
-    }
-};
 
-function createBlogPostsModel() {
-    const storage = Object.create(BlogPosts);
-    storage.posts = [];
-    return storage;
-}
+        // when DELETE request comes in with an id in path,
+        // try to delete that item from ShoppingList.
+        app.delete('/blog-posts/:id', (req, res) => {
+            ShoppingList.delete(req.params.id);
+            console.log(`Deleted item \`${req.params.ID}\``);
+            res.status(204).end();
+        });
 
-
-module.exports = {
-    BlogPosts: createBlogPostsModel()
-};
+        module.exports = {
+            router;
+        };
