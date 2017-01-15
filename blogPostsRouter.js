116423d2718c30d12eb1732cc47148f32fdfabf4
Faras Maroge <Goldencrane33@yahoo.com>
2017-01-15 00:12:19 -0500

    imported required properties

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -1,5 +1,15 @@
 const uuid = require('uuid');
 
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {
+    BlogPosts
+} = require('./models');
+
 // this module provides volatile storage, using a `BlogPost`
 // model. We haven't learned about databases yet, so for now
 // we're using in-memory storage. This means each time the app stops, our storage
@@ -10,64 +20,64 @@ const uuid = require('uuid');
 // is implemented, and getting it to use an existing model.
 
 function StorageException(message) {
-this.message = message;
-this.name = "StorageException";
+    this.message = message;
+    this.name = "StorageException";
 }
 
 const BlogPosts = {
-create: function (title, content, author, publishDate) {
-const post = {
-id: uuid.v4(),
-title: title,
-content: content,
-author: author,
-publishDate: publishDate || Date.now()
-};
-this.posts.push(post);
-return post;
-},
-get: function (id = /blog-posts) {
-// if id passed in, retrieve single post,
-// otherwise send all posts.
-if (id !== /blog-posts) {
-return this.posts.find(post => post.id === id);
-}
-// return posts sorted (descending) by
-// publish date
-return this.posts.sort(function (a, b) {
-return b.publishDate - a.publishDate
-});
-},
-delete: function (id) {
-const postIndex = this.posts.findIndex(
-post => post.id === id);
-if (postIndex > -1) {
-this.posts.splice(postIndex, 1);
-}
-},
-update: function (updatedPost) {
-const {
-id
-} = updatedPost;
-const postIndex = this.posts.findIndex(
-post => post.id === updatedPost.id);
-if (postIndex === -1) {
-throw StorageException(
-`Can't update item \`${id}\` because doesn't exist.`)
-}
-this.posts[postIndex] = Object.assign(
-this.posts[postIndex], updatedPost);
-return this.posts[postIndex];
-}
+    create: function (title, content, author, publishDate) {
+        const post = {
+            id: uuid.v4(),
+            title: title,
+            content: content,
+            author: author,
+            publishDate: publishDate || Date.now()
+        };
+        this.posts.push(post);
+        return post;
+    },
+    get: function (id = /blog-posts) {
+        // if id passed in, retrieve single post,
+        // otherwise send all posts.
+        if (id !== /blog-posts) {
+            return this.posts.find(post => post.id === id);
+        }
+        // return posts sorted (descending) by
+        // publish date
+        return this.posts.sort(function (a, b) {
+            return b.publishDate - a.publishDate
+        });
+    },
+    delete: function (id) {
+        const postIndex = this.posts.findIndex(
+            post => post.id === id);
+        if (postIndex > -1) {
+            this.posts.splice(postIndex, 1);
+        }
+    },
+    update: function (updatedPost) {
+        const {
+            id
+        } = updatedPost;
+        const postIndex = this.posts.findIndex(
+            post => post.id === updatedPost.id);
+        if (postIndex === -1) {
+            throw StorageException(
+                `Can't update item \`${id}\` because doesn't exist.`)
+        }
+        this.posts[postIndex] = Object.assign(
+            this.posts[postIndex], updatedPost);
+        return this.posts[postIndex];
+    }
 };
 
 function createBlogPostsModel() {
-const storage = Object.create(BlogPosts);
-storage.posts = [];
-return storage;
+    const storage = Object.create(BlogPosts);
+    storage.posts = [];
+    return storage;
 }
 
 
 module.exports = {
-BlogPosts: createBlogPostsModel()
+    BlogPosts: createBlogPostsModel()
 };
